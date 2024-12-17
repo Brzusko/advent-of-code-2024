5387c2fd709838c5631091f66a45cc6efecc9581
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-12-17 18:54:30 +0100

    Injecting library code into bin project

--- day1/bin/src/main.rs
+++ day1/bin/src/main.rs
@@ -1,3 +1,14 @@
+use lib::id_checker::check_ids_offset;
+
+// Solution https://adventofcode.com/2024/day/1
+
 fn main() {
-    println!("Hello, world!");
+    let left_ids: Vec<u32> = vec![3, 4, 2, 1, 3, 3];
+    let right_ids: Vec<u32> = vec![4, 3, 5, 3, 9, 3];
+    let result = check_ids_offset(&left_ids, &right_ids);
+    
+    match result {
+        Ok(valid_result) => { println!("Offset value: {}", valid_result) }
+        Err(error) => { println!("{}", error.to_string()) }
+    }
 }
