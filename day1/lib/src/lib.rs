bf1021009b3d073ab2e967b879cc8ffd142a7c63
Brzusko <wojciech.brzuszkiewicz@hotmail.com>
2024-12-17 01:49:21 +0100

    Traits setup

--- day1/lib/src/lib.rs
+++ day1/lib/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub mod id_checker
+{
+    use anyhow::*;
+    use std::ops::Sub;
+    
+    pub trait Integer {}
+
+    impl Integer for u8 {}
+    impl Integer for u16 {}
+    impl Integer for u32 {}
+    impl Integer for u64 {}
+    impl Integer for u128 {}
+    
+    impl Integer for i8 {}
+    impl Integer for i16 {}
+    impl Integer for i32 {}
+    impl Integer for i64 {}
+    impl Integer for i128 {}
+    
+    pub fn check_ids<T>() -> anyhow::Result<u32>
+    where
+        T: Copy,
+        T: Sub,
+        T: PartialEq,
+        T: Integer,
+    {
+        todo!()
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
